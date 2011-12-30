3d01d50c299cfb2bd78b9b8f0c4c71d35cf2faf6
fent <933490+fent@users.noreply.github.com>
2011-12-30 06:27:36 -0800

    tests now written in mocha

---
+++ test/emitter-test.js
@@ -0,0 +1,79 @@
+var p = require('..')
+  , assert = require('assert')
+  , EventEmitter = require('events').EventEmitter
+
+
+describe('Event Emitter', function() {
+  var ee = new EventEmitter()
+    , foo = bar = 0
+    , a, b, c
+
+  ee.on('foo', function(a2, b2) {
+    a = a2;
+    b = b2;
+    foo++;
+  });
+
+  ee.on('bar', function(a) {
+    c = a;
+    bar++;
+  });
+
+  describe('Emit', function() {
+
+    it('Emits correct event with arguments', function() {
+      ee.emit('foo', 1, 2);
+      ee.emit('bar', 'baz');
+
+      assert.equal(foo, 1);
+      assert.equal(a, 1);
+      assert.equal(b, 2);
+
+      assert.equal(bar, 1);
+      assert.equal(c, 'baz');
+    });
+
+    describe('Pause and emit events', function() {
+
+      it('Listeners do not get called', function() {
+        p.pause(ee);
+        ee.emit('foo', 'a', 'b');
+        ee.emit('bar', true);
+
+        assert.equal(foo, 1);
+        assert.equal(a, 1);
+        assert.equal(b, 2);
+
+        assert.equal(bar, 1);
+        assert.equal(c, 'baz');
+      });
+
+      describe('Resume and try emitting again', function() {
+
+        it('Buffered events call listeners', function() {
+          p.resume(ee);
+
+          assert.equal(foo, 2);
+          assert.equal(a, 'a');
+          assert.equal(b, 'b');
+
+          assert.equal(bar, 2);
+          assert.equal(c, true);
+        });
+
+        it('Events emitted call listeners immediately', function() {
+          ee.emit('foo', 'mom', 'dad');
+          ee.emit('bar', 'hello');
+
+          assert.equal(foo, 3);
+          assert.equal(a, 'mom');
+          assert.equal(b, 'dad');
+
+          assert.equal(bar, 3);
+          assert.equal(c, 'hello');
+        });
+      });
+
+    });
+  });
+});
---
+++ test/settimeout-test.js
@@ -0,0 +1,119 @@
+var p = require('..')
+  , assert = require('assert')
+
+
+describe('setTimeout', function() {
+  it('Calls given function after some time', function(done) {
+    var yes = false;
+    var timeout = p.setTimeout(function() {
+      yes = true;
+    }, 100);
+
+    setTimeout(function() {
+      assert.ok(yes);
+      assert.ok(!timeout.isPaused());
+      assert.ok(timeout.isDone());
+      done();
+    }, 101);
+  });
+
+  describe('Pause', function() {
+    var yes = false;
+    var timeout;
+
+    it('Does not call function', function(done) {
+      timeout = p.setTimeout(function() {
+        yes = true;
+      }, 200);
+
+      setTimeout(function() {
+        timeout.pause();
+      }, 100);
+
+      setTimeout(function() {
+        assert.ok(!yes);
+        assert.ok(timeout.isPaused());
+        assert.ok(!timeout.isDone());
+        done();
+      }, 100);
+    });
+
+    describe('Resume after some time', function() {
+      it('Calls function', function(done) {
+        setTimeout(function() {
+          timeout.resume()
+        }, 100);
+
+        setTimeout(function() {
+          assert.ok(yes);
+          assert.ok(!timeout.isPaused());
+          assert.ok(timeout.isDone());
+          done();
+        }, 250);
+      });
+    });
+
+  });
+
+  describe('Pause for some time', function() {
+    var yes = false;
+    var timeout;
+
+    it('Does not call function in time', function() {
+      timeout = p.setTimeout(function() {
+        yes = true;
+      }, 100);
+
+      timeout.pause(200);
+
+      assert.ok(!yes);
+      assert.ok(timeout.isPaused());
+      assert.ok(!timeout.isDone());
+    });
+
+    it('Calls function after resumed again', function(done) {
+      setTimeout(function() {
+        assert.ok(yes);
+        assert.ok(!timeout.isPaused());
+        assert.ok(timeout.isDone());
+        done();
+      }, 350);
+    });
+  });
+
+  describe('interchangeableArguments', function() {
+    var yes = false;
+    var timeout = p.setTimeout(30, function() {
+      yes = true;
+    });
+
+    it('Still calls function', function(done) {
+      setTimeout(function() {
+        assert.ok(yes);
+        assert.ok(timeout.isDone());
+        done();
+      }, 31);
+    });
+  });
+
+  describe('OnDone', function() {
+    var yes = false;
+    var ondone = false;
+    var timeout = p.setTimeout(function() {
+      yes = true;
+    }, 100);
+
+    timeout.onDone(function() {
+      ondone = true;
+    });
+
+    it('Function passed to onDone is called when done', function(done) {
+      setTimeout(function() {
+        assert.ok(yes);
+        assert.ok(ondone);
+        done();
+      }, 101);
+    });
+  });
+
+});
