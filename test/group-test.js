e6a0c70da8621c93c0d7c2f31a0619badce678bb
fent <933490+fent@users.noreply.github.com>
2017-07-16 20:23:20 -0400

    Use fake timers for tests

--- test/group-test.js
+++ test/group-test.js
@@ -5,25 +5,38 @@ var EventEmitter = require('events').EventEmitter;
 
 
 describe('Group', function() {
-  var g = p.createGroup();
-  var a = sinon.spy();
-  var b = sinon.spy();
-  var c = sinon.spy();
-  
-  var ee = g.add(new EventEmitter());
-  ee.on('a', a);
-  g.setInterval(function() {
-    ee.emit('a');
-  }, 100);
-  g.setInterval(b, 100);
-  g.setTimeout(c, 50);
+  var clock, mysetTimeout;
+  before(function() {
+    clock = sinon.useFakeTimers();
+    mysetTimeout = function(fn, ms) {
+      setTimeout(fn, ms);
+      clock.tick(ms);
+    };
+  });
+  after(function() { clock.restore(); });
+
+  var group, a, b, c, ee;
+  beforeEach(function() {
+    group = p.createGroup();
+    a = sinon.spy();
+    b = sinon.spy();
+    c = sinon.spy();
+    
+    ee = group.add(new EventEmitter());
+    ee.on('a', a);
+    group.setInterval(function() { ee.emit('a'); }, 100);
+    group.setInterval(b, 100);
+    group.setTimeout(c, 50);
+  });
+
+  afterEach(function() { group.clear(); });
 
   it('Has correct amount of timers', function() {
-    assert.equal(g.timers().length, 4);
+    assert.equal(group.timers().length, 4);
   });
 
   it('Correctly calls functions and listeners', function(done) {
-    setTimeout(function() {
+    mysetTimeout(function() {
       assert.equal(a.callCount, 1);
       assert.equal(b.callCount, 1);
       assert.ok(c.called);
@@ -33,29 +46,36 @@ describe('Group', function() {
 
   describe('Pause for a given time', function() {
     it('Calls will be delayed', function(done) {
-      g.pause(50);
-
-      setTimeout(function() {
-        assert.equal(a.callCount, 1);
-        assert.equal(b.callCount, 1);
+      group.pause(50);
+      mysetTimeout(function() {
+        assert.equal(a.callCount, 0);
+        assert.equal(b.callCount, 0);
         done();
       }, 100);
     });
+  });
 
-    describe('Clear', function() {
-      it('EventEmitter will still be there', function(done) {
-        g.clear();
-        ee.emit('a');
-
-        setTimeout(function() {
-          assert.equal(g.timers().length, 1);
-          assert.equal(a.callCount, 2);
-          assert.equal(b.callCount, 1);
-          assert.ok(c.called);
-          done();
-        }, 100);
-      });
+  describe('Clear', function() {
+    it('Timers are cleared', function(done) {
+      group.clear();
+      mysetTimeout(function() {
+        assert.ok(!a.called);
+        assert.ok(!b.called);
+        assert.ok(!c.called);
+        done();
+      }, 300);
     });
 
+    it('EventEmitter will still be there', function(done) {
+      group.clear();
+      ee.emit('a');
+      mysetTimeout(function() {
+        assert.equal(group.timers().length, 1);
+        assert.equal(a.callCount, 1);
+        assert.equal(b.callCount, 0);
+        assert.ok(!c.called);
+        done();
+      }, 100);
+    });
   });
 });
--- test/setinterval-test.js
+++ test/setinterval-test.js
@@ -4,13 +4,26 @@ var sinon  = require('sinon');
 
 
 describe('setInterval', function() {
-  var callback = sinon.spy();
-  var interval;
+  var clock, mysetTimeout;
+  before(function() {
+    clock = sinon.useFakeTimers();
+    mysetTimeout = function(fn, ms) {
+      setTimeout(fn, ms);
+      clock.tick(ms);
+    };
+  });
+  after(function() { clock.restore(); });
+
+  var callback, interval;
+  beforeEach(function() {
+    callback = sinon.spy();
+    interval = p.setInterval(callback, 100);
+  });
+
+  afterEach(function() { interval.clear(); });
 
   it('Calls given function on interval', function(done) {
-    interval = p.setInterval(callback, 100);
-
-    setTimeout(function() {
+    mysetTimeout(function() {
       assert.equal(callback.callCount, 1);
       done();
     }, 105);
@@ -20,35 +33,38 @@ describe('setInterval', function() {
     it('Does not call given function since it was paused', function(done) {
       interval.pause(50);
       var next = interval.next();
-      assert.ok(next >= 0 && next < 100);
+      assert.equal(next, 100);
 
-      setTimeout(function() {
+      mysetTimeout(function() {
         assert.ok(interval.isPaused());
         assert.equal(interval.next(), next);
       }, 10);
 
-      // callback call count is still 1 because interval was paused
-      setTimeout(function() {
-        assert.equal(callback.callCount, 1);
+      // Callback call count is still 0 because interval was paused.
+      mysetTimeout(function() {
+        assert.equal(callback.callCount, 0);
         assert.ok(!interval.isPaused());
         assert.ok(!interval.isDone());
         done();
-      }, 100); // this will be called after `done` is called on the parent
+      }, 100);
     });
 
     it('Eventually resumes and keeps calling function', function(done) {
-      setTimeout(function() {
+      interval.pause(50);
+      clock.tick(100);
+      mysetTimeout(function() {
         interval.clear();
-        assert.equal(callback.callCount, 2);
+        assert.equal(callback.callCount, 1);
         assert.ok(!interval.isPaused());
         assert.ok(interval.isDone());
         done();
       }, 75);
     });
 
-    it('Does not call again after being cleared', function(done) {
-      setTimeout(function() {
-        assert.equal(callback.callCount, 2);
+    it('Does not call after being cleared', function(done) {
+      interval.clear();
+      mysetTimeout(function() {
+        assert.equal(callback.callCount, 0);
         assert.ok(interval.isDone());
         done();
       }, 100);
@@ -58,20 +74,31 @@ describe('setInterval', function() {
 
   describe('Clear right after resuming', function() {
     it('Does not call given function at all', function(done) {
-      var callback = sinon.spy();
-      var interval = p.setInterval(callback, 10);
-
-      setTimeout(function() {
+      mysetTimeout(function() {
         assert.equal(callback.callCount, 1);
         interval.pause();
         interval.resume();
         interval.clear();
 
-        setTimeout(function() {
+        mysetTimeout(function() {
           assert.equal(callback.callCount, 1);
           done();
         }, 50);
-      }, 15);
+      }, 115);
+    });
+  });
+
+  describe('interchangeableArguments', function() {
+    it('Still calls function', function(done) {
+      var callback = sinon.spy();
+      var interval = p.setInterval(30, callback);
+
+      mysetTimeout(function() {
+        assert.ok(callback.called);
+        assert.ok(!interval.isDone());
+        interval.clear();
+        done();
+      }, 35);
     });
   });
 });
